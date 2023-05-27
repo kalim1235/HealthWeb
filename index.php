feb85d301e0d71225b41d5585fadeeb6b3812b4a
kalim1235 <132089199+kalim1235@users.noreply.github.com>
2023-05-27 09:55:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title> HealthCare app </title>
+  
+
+  <link href="style/animate.css/animate.min.css" rel="stylesheet">
+  <link href="style/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="style/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="style/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="style/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="style/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="style/remixicon/remixicon.css" rel="stylesheet">
+  <link href="style/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+ 
+  <link href="css/mystyle.css" rel="stylesheet">
+  <script src="js/script.js.js" type="text/javascript"></script>
+   
+
+</head>
+
+<body>
+
+ 
+  <div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex justify-content-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope"></i> <a href="mailto:contact@example.com">healthcare@example.com</a>
+        <i class="bi bi-phone"></i> +961 12345678
+        
+
+        
+
+
+      </div>
+      <div class="d-none d-lg-flex social-links align-items-center">
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> 
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 id="heading12" class="logo me-auto" style="font-family: Times New Roman ,serif;font-size: 40px;font-weight: bold; color:darkblue;">HEALTH</h1>
+    
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#home">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#doctors">Doctors</a></li>       
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Home Section ======= -->
+  <section id="home" class="d-flex align-items-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <h1 style="font-family: Times New Roman ,serif;font-size: 40px;font-weight: bold; color:darkblue;">Welcome to our <br> Healthcare Website</h1>
+       
+      <img src="https://www.azquotes.com/vangogh-image-quotes/84/98/Quotation-Hippocrates-Health-is-the-greatest-of-human-blessings-84-98-80.jpg" style="width: 80%; max-width: 80%; border-radius: 5%; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);"/>   
+      </div>
+      <div class="col-lg-6 d-flex justify-content-center align-items-center">
+        <img src="https://blog.ipleaders.in/wp-content/uploads/2020/01/Health-Insurance.jpg" width="60%" height="auto" alt="Healthcare Image">
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- End Home -->
+<br><br><br><br><br><br><br><br>
+  <main id="main">
+    
+    
+    
+    
+
+    
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h2
+            style="font-family: Times New Roman ,serif;
+      font-size: 32px;
+      font-weight: bold;
+      color: blue;
+      text-align: center;
+      margin-top: 20px;
+      margin-bottom: 10px;">ABOUT US</h2>
+          <strong><p>We are a team of specialized doctors, We can help you optimize your health and keeping you always uptodate on what is 
+            good for your health by offering you our special services. <br> In this guide video below , you can understand more how
+            our services works.</strong><br><span style="color:#ff0000; font-weight:bold">  Don't Forget to Chech it out !!! </span>
+
+          </p>
+        </div>
+
+        <div class="row">
+  <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative">
+    <img src="https://wallpaperaccess.com/full/185899.jpg" alt="Image" style="width: 150%; max-width: 140%; border-radius: 10%; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);">
+  </div>
+  <div class="col-xl-7 col-lg-6 d-flex justify-content-center align-items-center">
+    <a href="https://www.youtube.com" class="youtube-button" style="display: flex; align-items: left; justify-content: center; background-color: red; color: white; width: 120px; height: 120px; border-radius: 50%; text-decoration: none; font-size: 60px; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3); padding-left: 15px;">&#9658;</a>
+  </div>
+</div>
+
+
+
+
+
+      </div>
+    </section><!-- End About Section -->
+
+   <!-- ======= Counts Section ======= -->
+   <!-- End Counts Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Services</h2>
+          <p>Check out our special services .</p>
+        </div>
+
+        <div class="row">
+        
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box" onclick="openTelemedicine()">
+              <div class="icon"><i class="fas fa-video"></i></div>
+              <h4><a href="">Teleconsultation</a></h4>
+              <p>Access medical consultations remotely through video calls with our specialized doctors.</p>
+            </div>
+          </div>
+
+
+
+        
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="fas fa-hospital-user"></i></div>
+              <h4><a href="">Articles</a></h4>
+              <p>Read some educational articles and documents concerning health topics that may be really helpfull for you </p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <div class="icon"><i class="fas fa-hand-holding-usd"></i></div>
+              <h4><a href="">Crowdfunding</a></h4>
+              <p>Easily donates to persons inneeds , simply by accessing our Crowdfunding website. Every pennie counts.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="fas fa-vial"></i></div>
+              <h4><a href="">Lab Test</a></h4>
+              <p>Chech your Laboratoire Tests directly from your home in just a click .</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="fas fa-medkit"></i></div>
+              <h4><a href="">Medecine</a></h4>
+              <p>We help you find your medecine in the nearest pharmacies </p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="fas fa-user profile-icon"></i></div>
+              <h4><a href="">Profile</a></h4>
+              <p>Check and edit your personal informations through our app in a simple and fast way. </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Appointment Section ======= -->
+    <section> 
+    </section><!-- End Appointment Section -->
+
+    
+
+    <!-- ======= Doctors Section ======= -->
+    <section id="doctors" style="background: rgb(43, 0, 128)  ;" class="doctors">
+      <div class="container">
+
+        <div class="section-title">
+          <h2 style="color:aqua;">Doctors</h2>
+          <h4 style="color:aqua;">Meet our specialized doctors who can help you and provide you with some excepional healthcare services from different kind.</h4>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="https://www.freepnglogos.com/uploads/doctor-png/doctor-bulk-billing-doctors-chapel-hill-health-care-medical-3.png" width="40%" style="background-color: #ff0000;border: 2px solid black;border-radius: 10%; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4 style="color:aqua;">Rozan hart</h4>
+                <span style="color:aqua;">Chief Medical Officer</span>
+                <p style="color:aqua;">Expert in.......</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4 mt-lg-0">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="https://www.freepnglogos.com/uploads/doctor-png/doctor-bulk-billing-doctors-chapel-hill-health-care-medical-3.png" width="40%" style="background-color: green ;border: 2px solid black;border-radius: 10%; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4 style="color:aqua;">Patricia johnson</h4>
+                <span style="color:aqua;">cardiologist</span>
+                <p style="color:aqua;">Specialized in .........</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4">
+            <div class="member d-flex align-items-start">
+              <div class="pic"><img src="https://www.freepnglogos.com/uploads/doctor-png/doctor-bulk-billing-doctors-chapel-hill-health-care-medical-3.png" width="40%" style="background-color: #ff0000;border: 2px solid black;border-radius: 10%; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4 style="color:aqua;">Bratt Watson</h4>
+                <span style="color:aqua;">Cardiology</span>
+                <p style="color:aqua;">Expert in . . . . . .  </p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mt-4">
+            <div class="member d-flex align-items-start">
+            <div class="pic"><img src="https://www.freepnglogos.com/uploads/doctor-png/doctor-bulk-billing-doctors-chapel-hill-health-care-medical-3.png" width="40%" style="background-color: green;border: 2px solid black;border-radius: 10%; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);" class="img-fluid" alt=""></div>
+
+              <div class="member-info">
+                <h4 style="color:aqua;">Kevin Jones</h4>
+                <span style="color:aqua;">Neurosurgeon</span>
+                <p style="color:aqua;">Expert in .......</p>
+                <div class="social">
+                  <a href=""><i class="ri-twitter-fill"></i></a>
+                  <a href=""><i class="ri-facebook-fill"></i></a>
+                  <a href=""><i class="ri-instagram-fill"></i></a>
+                  <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+<!-- End Doctors Section -->
+
+   
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2 style="font-weight: bold; color:black">Contact us</h2>
+          <h4 style="font-weight: bold; color:black"> you can contact us via email . wtsp , phone or any platforms . </h4>
+        </div>
+      </div>
+
+      
+
+      <div class="container">
+        <div class="row mt-5">
+
+          <div class="col-lg-4">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4 style="font-weight: bold; color:black">Location:</h4>
+                <p style="font-weight: bold; color:black">Bsalim Hospital</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4 style="font-weight: bold; color:black">Email:</h4>
+                <p style="font-weight: bold; color:black">Healthcare@example.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4 style="font-weight: bold; color:black">Call:</h4>
+                <p style="font-weight: bold; color:black"> +961 12345678</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-8 mt-5 mt-lg-0">
+            <form method="Post" action="" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" id="message" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button>
+              <class="text-center"><button type="reset">Reset</button>
+            </div>
+              
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>HealthCARE</h3>
+              <p>
+                abc Hospital Road<br>
+                Bsalim, Lebanon<br><br>
+                <strong>Phone:</strong> +961 12345678<br>
+                <strong>Email:</strong> Healthcare@example.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="https://www.facebook.com/groups/577400563258704" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>  
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#about">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#services">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#doctor">Doctors</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+
+          
+
+          
+
+</div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span></span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="">abc</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ js/script.js.js
@@ -0,0 +1,23 @@
+
+
+    
+  function openTelemedicine() {
+    var url = "telemedicine.html";
+    alert("You wwil be going to telemedecine page");
+  window.open('css/TelemedecinePage.html', '_blank');
+}
+
+
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'en', // Set the language of your website
+    includedLanguages: 'fr,es', // Set the languages you want to include in the translation options
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+    autoDisplay: false,
+    multilanguagePage: true
+  }, 'google_translate_element');
+}
+
+
+
